babb4b25ff17911a507a17878154f439e40701df
Hongyu Yang <hongyu.yang@mendesgans.nl>
2023-02-01 22:22:39 +0100

    react

--- web/src/test/java/ApplicationTests.java
+++ web/src/test/java/ApplicationTests.java
@@ -1,10 +1,10 @@
-package yyy;
+
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
